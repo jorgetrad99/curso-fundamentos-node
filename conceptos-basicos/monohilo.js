b8a195a561367a2488eeb9b447832c9a1919a126
Jorge Trad <jetradl@gmail.com>
2021-08-12 14:32:07 -0500

    Single Thread: Design and Safety Implications

---
+++ conceptos-basicos/monohilo.js
@@ -0,0 +1,14 @@
+console.log('Hola Mundo');
+
+let i = 0;
+
+setInterval(() => {
+  //Esta función se ejecuta cuando le toque
+  console.log(i);
+  i++;
+  if (i === 5) {
+    var a = 3 + z;
+  }
+}, 1000);
+
+console.log('Segunda instrucción');
